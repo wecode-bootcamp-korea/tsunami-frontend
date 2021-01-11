d0badcacf737f1850e495316be008a5b82a111b8
unihit <unihit@naver.com>
2021-01-11 17:15:06 +0900

    Add: first commit. 초기세팅 완료

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// 컴포넌트
+import Login from "./Pages/Login/Login";
+import Main from "./Pages/Main/Main";
+
+// import 한 컴포넌트에 대한 경로
+export default class Routes extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/login" conponent={Login} />
+          <Route exact path="/main" conponent={Main} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default Routes;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+// export const SERVER = "";
+
+// export const SIGNUP_API = `${SERVER}/account/`;
+
+// export const SIGNIN_API = `${SERVER}/account/login`;
+
