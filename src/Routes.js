72237141aa2b8e0493a1d64f60a08fab97c5c325
unihit <unihit@naver.com>
2021-01-11 18:34:16 +0900

    Modify: .eslintcache, Routes.js, Main.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // 컴포넌트
-import Login from "./Pages/Login/Login";
+import Signin from "./Pages/Signin/Signin";
 import Main from "./Pages/Main/Main";
 
 // import 한 컴포넌트에 대한 경로
@@ -11,12 +11,10 @@ export default class Routes extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/login" conponent={Login} />
-          <Route exact path="/main" conponent={Main} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/main" component={Main} />
         </Switch>
       </Router>
     );
   }
 }
-
-export default Routes;
