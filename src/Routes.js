c34a5f7b6fc477ed6890dd2ec56cfd98430a106e
unihit <unihit@naver.com>
2021-01-11 20:27:45 +0900

    Add: featrue/product branch and pages setting

--- src/Routes.js
+++ src/Routes.js
@@ -3,7 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // 컴포넌트
 import Signin from "./Pages/Signin/Signin";
-import Main from "./Pages/Main/Main";
+import Signup from "./Pages/Signup/Signup";
+import Stationery from "./Pages/Stationery/Stationery";
+import Productlist from "./Pages/Productlist/Productlist";
+import Productdetail from "./Pages/Productdetail/Productdetail";
 
 // import 한 컴포넌트에 대한 경로
 export default class Routes extends Component {
@@ -12,7 +15,10 @@ export default class Routes extends Component {
       <Router>
         <Switch>
           <Route exact path="/signin" component={Signin} />
-          <Route exact path="/main" component={Main} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/stationery" component={Stationery} />
+          <Route exact path="/productlist" component={Productlist} />
+          <Route exact path="/productdetail" component={Productdetail} />
         </Switch>
       </Router>
     );
