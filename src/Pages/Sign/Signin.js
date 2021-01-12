e9c8342704ab2c801c5308d79da8a0af014b6826
Hoic <hoicryu@gmail.com>
2021-01-12 22:00:30 +0900

    signin page1 update

--- src/Pages/Sign/Signin.js
+++ src/Pages/Sign/Signin.js
@@ -5,59 +5,56 @@ class Signin extends Component {
   render() {
     return (
       <div className="signin">
-        <div className="login">로그인</div>
-        <div className="member_wrapper">
-          <div className="member_box">
+        <div className="header">로그인</div>
+        <div className="memberWrapper">
+          <div className="memberBox">
             <div className="member">회원</div>
-            <div className="unmember">비회원 (주문조회)</div>
+            <div className="member unmember">비회원 (주문조회)</div>
           </div>
-          <div className="id_box">
+          <div className="idBox">
             <span className="id">아이디</span>
-            <input type="text" placeholder="아이디 입력" className="id_input" />
+            <input type="text" placeholder="아이디 입력" className="id Input" />
           </div>
-          <div className="pw_box">
+          <div className="pwBox">
             <span className="pw">비밀번호</span>
             <input
               type="password"
               placeholder="비밀번호 입력"
-              className="pw_input"
+              className="pw Input"
             />
           </div>
-          <button type="submit" className="login_btn">
+          <button type="submit" className="loginBtn">
             로그인
           </button>
-          <div className="sign_helper_box">
-            <p className="id_find">아이디 찾기</p>
-            <p className="pw_find">비밀번호 찾기</p>
-            <p className="goto_signup">회원가입</p>
+          <div className="signHelperBox">
+            <p className="idFind">아이디 찾기</p>
+            <p className="pwFind">비밀번호 찾기</p>
+            <p className="gotoSignup">회원가입</p>
           </div>
-          <div className="sns_wrapper">
-            <div className="sns_login">SNS 로그인</div>
-            <div className="sns_images_wrapper">
-              <div className="naver_box">
+          <div className="snsWrapper">
+            <div className="snsLogin">SNS 로그인</div>
+            <div className="snsImagesWrapper">
+              <div className="Box naver">
                 <img
                   src="/images/naver.png"
                   alt="never"
-                  width="55"
-                  className="naver_logo"
+                  className="Logo naver"
                 />
                 <p className="naver">NAVER</p>
               </div>
-              <div className="kakao_box">
+              <div className="Box kakao">
                 <img
                   src="/images/kakao.png"
                   alt="gagao"
-                  width="55"
-                  className="kakao_logo"
+                  className="Logo kakao"
                 />
                 <p className="kakao">KAKAO</p>
               </div>
-              <div className="facebook_box">
+              <div className="Box facebook">
                 <img
                   src="/images/facebook.png"
                   alt="acebook"
-                  width="55"
-                  className="facebook_logo"
+                  className="Logo facebook"
                 />
                 <p className="facebook">FACEBOOK</p>
               </div>
