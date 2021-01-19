72ea8329ed50ed69692e943805d6862499975739
Hwnag In Tae <unihit@naver.com>
2021-01-19 09:16:33 +0900

    페이지 favicon 적용되게 변경

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // 컴포넌트
-import Signin from "./Pages/Signin/Signin";
-import Signup from "./Pages/Signup/Signup";
 import Stationery from "./Pages/Stationery/Stationery";
 import Productlist from "./Pages/Productlist/Productlist";
 import Signin from "./Pages/Sign/Signin";
 import Signup from "./Pages/Sign/Signup";
-import Stationery from "./Pages/Stationery/Stationery";
 import Productdetail from "./Pages/Productdetail/Productdetail";
 
 // import 한 컴포넌트에 대한 경로
